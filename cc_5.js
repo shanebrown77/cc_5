11b4d997b9dcfeee06b6909b20ce092cb0a1ccd9
shanebrown77 <Bds1@usf.edu>
2025-09-22 13:03:06 -0400

    inital commit step 2 added

---
+++ cc_5.js
@@ -0,0 +1,12 @@
+// coding challenge 5
+
+// step 2
+
+let employees = [
+
+{name: "Jamie James" , hourlyRate: 20 , hoursWorked: 20        }, 
+{name: "Tabitha Helen" , hourlyRate: 30 , hoursWorked: 15      },        
+{name: "Thomas Shields" , hourlyRate: 15.25 , hoursWorked: 18  },
+{name: "Palinko pinko" , hourlyRate: 90.50 , hoursWorked: 13   },
+
+]
