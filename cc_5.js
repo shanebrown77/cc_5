8eeb83ff52d550f2c59b4116df9dc44984813dfe
shanebrown77 <Bds1@usf.edu>
2025-09-28 22:15:58 -0400

    final commit

--- cc_5.js
+++ cc_5.js
@@ -6,10 +6,10 @@ console.log(`---inital employee array with hours worked and rate---`)
 
 let employees = [
 
-{name: "Jamie James" , hourlyRate: 20 , hoursWorked: 20        }, 
-{name: "Tabitha Helen" , hourlyRate: 30 , hoursWorked: 15      },        
-{name: "Thomas Shields" , hourlyRate: 15.25 , hoursWorked: 18  },
-{name: "Palinko pinko" , hourlyRate: 90.50 , hoursWorked: 13   },
+{name: "Jamie James" , hourlyRate: 20 , hoursWorked: 100        }, 
+{name: "Tabitha Helen" , hourlyRate: 30 , hoursWorked: 200      },        
+{name: "Thomas Shields" , hourlyRate: 15.25 , hoursWorked: 130  },
+{name: "Palinko Pinko" , hourlyRate: 90.50 , hoursWorked: 76   },
 
 ]
 console.log(employees);
@@ -22,21 +22,40 @@ function calculateBasePay (rate, hours) {
         return hours * rate;
 
     } else {
-    return 40 * rate;
+        return 40 * rate;
     }
 
 }
 
 // calculating OVertime/step 4
 
-
+function calculateOvertimePay (rate, hours) {
+    overtimeHours = hours - 40
+        return overtimeHours * (rate *1.5);
+}
 
 // calculating taxes/step 5
 
-
+function calculateTaxes (grossPay) {
+    tax = 0.15 * grossPay
+    return grossPay - tax
+}
 
 // process payroll/step 6 
+console.log(`\n---process payroll---`)
 
-
+function processPayroll (employee) {
+let basePay = calculateBasePay(employee.hourlyRate, employee.hoursWorked);
+let overtimePay = calculateOvertimePay(employee.hourlyRate, employee.hoursWorked);
+let grossPay = (basePay+overtimePay);
+let netPay = calculateTaxes(grossPay);
+return {
+name: employee.name, basePay: basePay, overtimePay: overtimePay, grossPay: grossPay, netPay: netPay
+};
+};
 
 //looping through array/step 7 
+
+employees.forEach(employee => {
+console.log(processPayroll(employee));
+});
